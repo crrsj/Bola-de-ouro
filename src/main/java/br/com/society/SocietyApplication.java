8dbef52beb306e1481ae52c061abc9e3bb039def
Carlos Roberto Ribeiro Santos Junior <138411850+crrsj@users.noreply.github.com>
2025-02-25 10:46:17 -0300

    Update SocietyApplication.java

--- src/main/java/br/com/society/SocietyApplication.java
+++ src/main/java/br/com/society/SocietyApplication.java
@@ -3,19 +3,9 @@ package br.com.society;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "API - Bola De Ouro.",
-				version = "1.0",
-				description = "API ALUGUEL DE CAMPO SOCIETY .",
-				contact = @Contact(name = "Carlos Roberto", email = "crrsj1@gmail.com")
-		)
-)
 public class SocietyApplication {
 
 	public static void main(String[] args) {
